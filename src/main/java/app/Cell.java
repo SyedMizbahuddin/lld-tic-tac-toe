5eb4a36bf648515dca0f248fe55cf1c92af1a438
Syed Mizbahuddin <syed@nworx.ai>
2024-08-09 14:51:28 +0530

    Initial commit

---
+++ src/main/java/app/Cell.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Cell {
+
+	Player player;
+	// additional details
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+}
---
+++ src/main/java/app/Player.java
@@ -0,0 +1,21 @@
+package app;
+
+public class Player {
+	String name;
+	Character symbol;
+
+	public Player(String name, Character symbol) {
+		super();
+		this.name = name;
+		this.symbol = symbol;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Character getSymbol() {
+		return symbol;
+	}
+
+}
